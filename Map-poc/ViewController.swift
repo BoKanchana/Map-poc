0df5fcce73531b2315bdb64ea8e91b7b21f58e37
Kanchana Phakdeedorn <kanchana.phakdeedorn>
2020-04-22 16:15:04 +0700

    Initial Commit

---
+++ Map-poc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map-poc
+//
+//  Created by KANCHANA PHAKDEEDORN on 22/4/2563 BE.
+//  Copyright © 2563 KANCHANA PHAKDEEDORN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
